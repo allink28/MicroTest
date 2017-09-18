ae76205a3d9628295eccea6ef22d0191f05027b2
allenpreville <allen.preville@xorsecurity.com>
2017-09-18 10:55:07 -0400

    Initial JHipster Micro Service generation with Kafka

---
+++ src/main/java/com/xorsecurity/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data Elasticsearch repositories.
+ */
+package com.xorsecurity.repository.search;
---
+++ src/main/java/com/xorsecurity/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.xorsecurity.web.rest.vm;
